9119150e6cc641859fd6ae89a044ca4135550368
Kaynecoirir <arseniy.grodno@mail.ru>
2023-09-08 23:10:53 +0300

    to the hell json library, simple funct to read file

--- Assets/App/Scripts/Scenes/SceneWordSearch/Features/Level/BuilderLevelModel/ProviderWordLevel/ProviderWordLevel.cs
+++ Assets/App/Scripts/Scenes/SceneWordSearch/Features/Level/BuilderLevelModel/ProviderWordLevel/ProviderWordLevel.cs
@@ -1,8 +1,5 @@
 using System;
 using System.IO;
-using System.Text.Encodings.Web;
-using System.Text.Json;
-using System.Text.Unicode;
 using App.Scripts.Scenes.SceneWordSearch.Features.Level.Models.Level;
 
 namespace App.Scripts.Scenes.SceneWordSearch.Features.Level.BuilderLevelModel.ProviderWordLevel
@@ -11,20 +8,34 @@ namespace App.Scripts.Scenes.SceneWordSearch.Features.Level.BuilderLevelModel.Pr
     {
         public LevelInfo LoadLevelData(int levelIndex)
         {
-            var options1 = new JsonSerializerOptions
-            {
-                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
-                WriteIndented = false
-            };
             string file_text = File.ReadAllText("Asset/App/Resources/Fillwords/" + levelIndex + ".json");
-            Lib fl = JsonSerializer.Deserialize<Lib>(file_text, options1);
             LevelInfo li = new LevelInfo();
-            li.words = new System.Collections.Generic.List<string>(fl.words);
+            li.words = new System.Collections.Generic.List<string>(GetStringFromJson(file_text));
             return li;
         }
-        class Lib
+        static System.Collections.Generic.List<string> GetStringFromJson(string line)
         {
-            public string[] words { get; set; }
+            System.Collections.Generic.List<string> list = new System.Collections.Generic.List<string>();
+            char c;
+            int index = 0;
+            bool isWords = false;
+            while (index != line.Length - 1)
+            {
+                c = line[index++];
+                if (c == '[') isWords = true;
+                if (isWords && c == '\"')
+                {
+                    c = line[index++];
+                    string word = "";
+                    while (c != '\"' && c != '\0')
+                    {
+                        word += c;
+                        c = line[index++];
+                    }
+                    list.Add(word);
+                }
+            }
+            return list;
         }
     }
 }
